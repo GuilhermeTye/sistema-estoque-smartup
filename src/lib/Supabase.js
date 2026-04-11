cd70ff56fc4704629d406ed83d6b537f69798c3f
Guilherme Dutra <dutratecnologies@gmail.com>
2026-04-11 11:24:53 -0300

    Preparando deploy

--- src/lib/Supabase.js
+++ src/lib/Supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ctmsxbcalskxzotvznem.supabase.co";
-const supabaseAnonKey = "sb_publishable_qZRSOSw7_4QhTTHQxOLzqg_KQXqzlXh";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 /*
 NEXT_PUBLIC_SUPABASE_URL=https://ctmsxbcalskxzotvznem.supabase.co
